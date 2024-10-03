50785358200c412b838e1e117e9ba16de4797952
olegok111 <olegokoyomov2@gmail.com>
2024-10-03 09:28:00 +0300

    [Task 03] Minimal version DONE Add delay argument

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -4,7 +4,14 @@ y=0
 while read -r -N 1 char; do
     char_octal=$(echo -ne "$char" | od -b -A none)
     char_octal=${char_octal# }
-    echo "tput cup $y $x; printf '\\$char_octal'" >> $tputcomfile
+    echo -n "tput cup $y $x; printf '\\$char_octal'" >> $tputcomfile
+
+    if [ $1 ]; then
+        echo "; sleep $1" >> $tputcomfile
+    else
+        echo >> $tputcomfile
+    fi
+
     if [ "$char_octal" = '012' ]; then
         y=$((y+1))
         x=0
