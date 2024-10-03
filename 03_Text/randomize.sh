0552e3366da510ca635f381771e5d4b8d735bef1
olegok111 <olegokoyomov2@gmail.com>
2024-10-03 09:08:22 +0300

    [Task 03] WIP 2: fix backslash treatment with the help of read's -r flag

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -1,14 +1,9 @@
 tputcomfile=$(mktemp)
 x=0
 y=0
-while read -N 1 char; do
-    if [ "$char" = "\\" ]; then
-        echo Gotcha! >> /tmp/backslashes
-        char_octal='134'
-    else
-        char_octal=$(echo -ne "$char" | od -b -A none)
-        char_octal=${char_octal# }
-    fi
+while read -r -N 1 char; do
+    char_octal=$(echo -ne "$char" | od -b -A none)
+    char_octal=${char_octal# }
     echo "tput cup $y $x; printf '\\$char_octal'" >> $tputcomfile
     if [ "$char_octal" = '012' ]; then
         y=$((y+1))
@@ -21,5 +16,5 @@ echo tput clear > ${tputcomfile}2
 shuf $tputcomfile >> ${tputcomfile}2
 chmod +x ${tputcomfile}2
 ${tputcomfile}2
-rm $tputcomfile ${tputcomfile}2
+rm -f $tputcomfile ${tputcomfile}2
 tput cup $((y+1)) 0
