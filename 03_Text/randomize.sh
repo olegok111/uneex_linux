d5317c9bea2acd382c2a2295b0f02e3138777550
olegok111 <olegokoyomov2@gmail.com>
2024-10-07 14:40:42 +0300

    [Task 03] Switch to reading line-by-line

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -1,23 +1,22 @@
 tputcomfile=$(mktemp)
-x=0
 y=0
-while read -r -N 1 char; do
-    char_octal=$(echo -ne "$char" | od -b -A none)
-    char_octal=${char_octal# }
-    echo -n "tput cup $y $x; printf '\\$char_octal'" >> $tputcomfile
+while IFS= read -r str; do
+    x=0
+    str_octal=$(echo -n "$str" | od -b -A none)
+    str_octal=${str_octal# }
 
-    if [ $1 ]; then
-        echo "; sleep $1" >> $tputcomfile
-    else
-        echo >> $tputcomfile
-    fi
-
-    if [ "$char_octal" = '012' ]; then
-        y=$((y+1))
-        x=0
-    else
+    for octbyte in $str_octal; do
+        echo -n "tput cup $y $x; printf '\\$octbyte'" >> $tputcomfile
         x=$((x+1))
-    fi
+
+        if [ $1 ]; then
+            echo "; sleep $1" >> $tputcomfile
+        else
+            echo >> $tputcomfile
+        fi
+    done
+
+    y=$((y+1))
 done
 echo tput clear > ${tputcomfile}2
 shuf $tputcomfile >> ${tputcomfile}2
